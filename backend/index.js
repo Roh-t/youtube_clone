c3b3df2a53ba5db4e25080ac6ef641d3b8b3938c
Rohit Singh <singh385536@gmail.com>
2026-07-22 00:45:01 +0530

    added deployed urls

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-   origin:"http://localhost:5173",
+   origin:"https://youtube-clone-blue-tau.vercel.app",
    credentials:true
 }))
 
