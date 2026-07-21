cd835b7ccd8904d548f746dd02e45f9d9757f78f
Rohit Singh <singh385536@gmail.com>
2026-07-22 01:25:34 +0530

    revert changes

--- backend/index.js
+++ backend/index.js
@@ -8,31 +8,28 @@ import userRouter from './route/userRoute.js'
 import contentRouter from './route/contentRoute.js'
 
 dotenv.config()
+const port = process.env.PORT
+
 
 const app = express()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-   origin: "https://youtube-clone-blue-tau.vercel.app",
-   credentials: true
+   origin:"https://youtube-clone-blue-tau.vercel.app",
+   credentials:true
 }))
 
-app.use("/api/auth", authRouter)
-app.use("/api/user", userRouter)
-app.use("/api/content", contentRouter)
 
-app.get("/", (req, res) => {
+
+app.use("/api/auth",authRouter)
+app.use("/api/user",userRouter)
+app.use("/api/content",contentRouter)
+
+app.get("/" , (req,res)=>{
     res.send("Hello from Server")
 })
 
-connectDb() // DB connect karo top level pe, listen ke andar nahi
-
-// Vercel ke liye — app.listen() sirf local development ke liye
-if (process.env.NODE_ENV !== "production") {
-    const port = process.env.PORT || "https://youtube-clone-12z9.vercel.app"
-    app.listen(port, () => {
-        console.log("Server Started on", port)
-    })
-}
-
-export default app  // Vercel isko serverless function jaise use karega
+app.listen(port , ()=>{
+    console.log("Server Started")
+    connectDb()
+})
